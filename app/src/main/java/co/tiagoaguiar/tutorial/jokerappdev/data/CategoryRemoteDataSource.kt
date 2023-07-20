5aa9afbf0cc7ac0e28bea38bfc076ba720c20264
Riann Silva <ooriannsilva@gmail.com>
2023-07-19 23:21:43 -0300

    Modern Library 3: HTTP Client with Retrofit

--- app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/data/CategoryRemoteDataSource.kt
+++ app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/data/CategoryRemoteDataSource.kt
@@ -1,23 +1,36 @@
 package co.tiagoaguiar.tutorial.jokerappdev.data
 
-import android.os.Handler
-import android.os.Looper
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class CategoryRemoteDataSource {
 
-    fun findAllCategories(callback: ListCategoryCallback){
-        Handler(Looper.getMainLooper()).postDelayed({
-            val response = arrayListOf(
-                "categoria 1",
-                "categoria 2",
-                "categoria 3",
-                "categoria 4"
-            )
+    fun findAllCategories(callback: ListCategoryCallback) {
+        HTTPClient.retrofit()
+            .create(ChuckNorrisAPI::class.java)
+            .findAllCategories(HTTPClient.API_KEY)
+            .enqueue(object : Callback<List<String>> {
+                override fun onResponse(
+                    call: Call<List<String>>,
+                    response: Response<List<String>>
+                ) {
+                    if (response.isSuccessful) {
+                        val categories = response.body()
+                        callback.onSuccess(categories ?: emptyList())
+                    } else {
+                        //quando server devolve status de error < 500
+                        val error = response.errorBody()?.toString()
+                        callback.onError(error ?: "Erro desconhecido")
+                    }
+                    callback.onComplete()
+                }
 
-            callback.onSuccess(response)
-//            onError("Falha na conexão, tente novamente mais tarde.")
-//
-            callback.onComplete()
-        }, 2000)
+                override fun onFailure(call: Call<List<String>>, t: Throwable) {
+                    callback.onError(t.message ?: "Erro interno")
+                    callback.onComplete()
+                }
+
+            })
     }
 }
---
+++ app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/data/HTTPClient.kt
@@ -0,0 +1,31 @@
+package co.tiagoaguiar.tutorial.jokerappdev.data
+
+import co.tiagoaguiar.tutorial.jokerappdev.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object HTTPClient {
+
+    private const val BASE_URL = "https://api.tiagoaguiar.co/jokerapp/"
+    const val API_KEY = "26cf6936-a2b8-4ebd-8f9b-095d4d9c7e70"
+
+    fun retrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(httpClient())
+            .build()
+    }
+
+    private fun httpClient(): OkHttpClient {
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+
+        return OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+    }
+
+}
